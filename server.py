200eca3bee1c0e5f9f0e3b0f879bec77a4202bbe
jaemin <jmcylove@gmail.com>
2023-11-21 10:06:50 +0900

    add POST /video

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -9,6 +11,18 @@ def root():
         data = request.json
         print(data)
         return jsonify({'message': 'POST request received', 'data': data})
+
+
+@app.route('/video', methods=['POST'])
+def video():
+    if request.method == 'POST':
+        # Save the binary data to a file
+        data = request.get_data()
+        save_path = 'media/'  # Change this to the desired directory
+        os.makedirs(save_path, exist_ok=True)
+        with open(os.path.join(save_path, 'video.3gp'), 'wb') as f:
+            f.write(data)
+        return jsonify({'message': 'POST request received'})
 
 
 app.run(host='0.0.0.0')
